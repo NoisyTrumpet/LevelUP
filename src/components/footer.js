45696ae9562342d379db496fe373aeb6ab3f95f7
Heather <heather.l.preiss@gmail.com>
2019-09-10 13:21:31 -0500

    design updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,11 @@ import React from "react"
 import PropTypes from "prop-types"
 
 // import { COLORS } from "../styles/constants"
-import Logo from "../images/LevelUp-Logo.png"
+import Facebook from "../images/icons/facebook.svg"
+import Instagram from "../images/icons/instagram.svg"
+import Twitter from "../images/icons/twitter.svg"
+import Linkedin from "../images/icons/linkedin.svg"
+import Logo from "../images/LevelUP-Logo.png"
 
 import "../styles/footer.css"
 
@@ -12,9 +16,29 @@ const Footer = ({ siteTitle }) => (
       <img src={Logo} alt="" />
     </div>
     <div className="footSocial">
-      <img src={Logo} alt="" />
-      <img src={Logo} alt="" />
-      <img src={Logo} alt="" />
+      <img src={Facebook} alt="" />
+      <img src={Instagram} alt="" />
+      <img src={Twitter} alt="" />
+      <img src={Linkedin} alt="" />
+    </div>
+    <div className="footerLinks">
+      <ul className="footerLinks1">
+        <li><a href="/">RSS</a></li>
+        <li><a href="/">IG</a></li>
+        <li><a href="/">FOIA</a></li>
+        <li><a href="/">SAPR</a></li>
+        <li><a href="/">USA.gov</a></li>
+        <li><a href="/">Accessibility</a></li>
+        <li><a href="/">AF Sites</a></li>
+      </ul>
+      <ul className="footerLinks2">
+        <li><a href="/">Link Disclaimer</a></li>
+        <li><a href="/">AF Executive Fleet</a></li>
+        <li><a href="/">No FEAR Act</a></li>
+        <li><a href="/">Strategic APR</a></li>
+        <li><a href="/">Suicide Prevention</a></li>
+        <li><a href="/">EEO</a></li>
+      </ul>
     </div>
   </footer>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -52,9 +52,7 @@ const IndexPage = () => (
     <Federal />
     <Center />
     <CallToAction />
-    <Center />
     <Contact />
-    <Center />
   </Layout>
 )
 
