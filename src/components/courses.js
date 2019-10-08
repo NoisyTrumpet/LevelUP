0a8f8986ea995ab989c8de479760c22080358d66
Heather <heather.l.preiss@gmail.com>
2019-10-08 12:57:45 -0500

    Update to Courses, added react-tabs-redux

--- src/components/courses.js
+++ src/components/courses.js
@@ -9,14 +9,18 @@ import CirComp from "../images/cirCompIcon.png"
 import BlkComp from "../images/blkCompIcon.png"
 import HandIcon from "../images/handIcon.png"
 
+import { Tabs, TabContent, TabLink } from "react-tabs-redux"
+import ScrollAnimation from "react-animate-on-scroll"
+
+
 import '../styles/courses.css'
 
 const Courses = () => (
-  <div id="courses" className="courseMain">
+<div id="courses" className="courseMain">
     
       <h1 className="coursesSect" style={{color: COLORS.levelUpPurple}}>Code 
       <span style={{fontSize: "2.5rem", color: COLORS.levelUpBlue}}> for the</span> win</h1>
-
+<Tabs renderActiveTabContentOnly={true}>
 <div className="cardSectCourse">
    <content
       style={{
@@ -29,29 +33,64 @@ const Courses = () => (
       }}
     >
 
-      <div className="skewCardCourse">
-        <div className="cardContCourse">
-          <p>LevelUP’s directive is to serve as a software factory 
-          to support a full range of Air Force and DoD missions.</p>
-        </div>
-      </div>
+        <TabContent for="tab1">
+            <div className="skewCardCourse1">
+            <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
+              <div className="cardContCourse">
+                <p><strong>LevelUP’s</strong> directive is to serve as a software factory 
+                  to support a full range of Air Force and DoD missions.</p>
+              </div>
+            </ScrollAnimation>
+            </div>
+        </TabContent>
+        <TabContent for="tab2">
+            <div className="skewCardCourse2">
+            <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
+              <div className="cardContCourse">
+                <p>Although our initial area of focus supports a range of Air Force missions,
+                  LevelUP will broaden its software development services to other functional areas.
+                  This includes a Unified Platform that will allow greater operational capabilities between
+                  the U.S. Air Force, U.S. Army, U.S. Navy, U.S. Marines and U.S. Coast Guard.
+                </p>
+              </div>
+              </ScrollAnimation>
+            </div>
+        </TabContent>
+        <TabContent for="tab3">
+            <div className="skewCardCourse3">
+            <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
+              <div className="cardContCourse">
+                <p>By developing software using agile processes and the latest DevSecOps technology,
+                  we're creating innovative capabilities at the scale and speed of need. 
+                </p>
+              </div>
+              </ScrollAnimation>
+            </div>
+        </TabContent>
 
       <div className="skewCardBackCourse">
         <div className="courses">
-          <div className="courseItem">
-            <img src={BlkComp} alt="" />
-            <p>Mission</p>
-          </div>
-          <img src={BodyLine} alt="" className="coursesDivide" />
-          <div className="courseItem">
-            <img src={CirComp} alt="" />
-            <p>Focus</p>
-          </div>
-          <div className="courseSpacer" />
-          <div className="courseItem">
-            <img src={HandIcon} alt="" />
-            <p>Real Time<br/>Strategy</p>
-          </div>
+        <TabLink to="tab1">
+            <div className="courseItem">
+              <img src={BlkComp} alt="" />
+              <p>Mission</p>
+            </div>
+          </TabLink>
+          <br/>
+          <TabLink to="tab2">
+            <div className="courseItem">
+              <img src={CirComp} alt="" />
+              <p>Focus</p>
+            </div>
+          </TabLink>
+          <br/>
+          <TabLink to="tab3">
+            <div className="courseItem">
+              <img src={HandIcon} alt="" />
+              <p>Real Time<br/>Strategy</p>
+            </div>
+          </TabLink>
+
         </div>
       </div>
 
@@ -59,39 +98,79 @@ const Courses = () => (
   </div>
 
       <div className="mobBackCourses">
-        <div className="mobContCourses">
-          <img src={MissionImg} alt="" />
-          <p><strong>LevelUP’s</strong> directive is to serve as a software factory 
-          to support a full range of Air Force and DoD missions.
-          </p>
-        </div>
+
+        <TabContent for="tab1">
+            <div className="mobContCourses">
+            <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
+              <img src={MissionImg} alt="" />
+              <p><strong>LevelUP’s</strong> directive is to serve as a software factory 
+              to support a full range of Air Force and DoD missions.
+              </p>
+            </ScrollAnimation>
+            </div>
+        </TabContent>
+        <TabContent for="tab2">
+            <div className="mobContCourses">
+            <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
+              <img src={MissionImg} alt="" />
+              <p>Although our initial area of focus supports a range of Air Force missions,
+              LevelUP will broaden its software development services to other functional areas.
+              This includes a Unified Platform that will allow greater operational capabilities between
+              the U.S. Air Force, U.S. Army, U.S. Navy, U.S. Marines and U.S. Coast Guard.
+              </p>
+            </ScrollAnimation>
+            </div>
+        </TabContent>
+        <TabContent for="tab3">
+            <div className="mobContCourses">
+            <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
+              <img src={MissionImg} alt="" />
+              <p>By developing software using agile processes and the latest DevSecOps technology,
+                  we're creating innovative capabilities at the scale and speed of need.
+              </p>
+            </ScrollAnimation>
+            </div>
+        </TabContent>
+
         <div className="mobSectCourses">
         <ul>
           <li>
+          <TabLink to="tab1">
             <div className="courseItem">
               <img src={BlkComp} alt="" />
               <p>Mission</p>
             </div>
+            </TabLink>
           </li>
           <img src={BodyLine} alt="" className="coursesDivide" />
+
           <li>
+          <TabLink to="tab2">
             <div className="courseItem">
               <img src={CirComp} alt="" />
               <p>Focus</p>
             </div>
+            </TabLink>
           </li>
-          <div className="courseSpacer" />
+          <img src={BodyLine} alt="" className="coursesDivide" />
+
           <li>
+          <TabLink to="tab3">
             <div className="courseItem">
               <img src={HandIcon} alt="" />
               <p>Real Time<br/>Strategy</p>
             </div>
+            </TabLink>
           </li>
+          <img src={BodyLine} alt="" className="coursesDivide" />
         </ul>
         </div>
       </div>
 
+
+    </Tabs>
   </div>
+
 )
 
 export default Courses
