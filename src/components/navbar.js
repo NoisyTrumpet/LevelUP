9171e1f50fa4261a7829bf6df49b2a8c72aebc84
Alexandra Fossum <alexandra.fossum@Ryannes-MacBook-Pro.local>
2019-10-08 08:29:28 -0500

    navbar and careers fixed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,11 @@
 import React from "react"
+import $ from "jquery"
 
 // import DrawerToggle from "./drawer-toggle"
 import NavLine from "../images/navLine.png"
 import Logo from "../images/LevelUp-Logo.png"
+
+// import * as Scroll from 'react-scroll';
 
 import '../styles/navbar.css'
 
@@ -10,8 +13,23 @@ const Navbar = () => (
 
   // $(window).scroll(function(){
   //   $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-  // });
+  // }),
 
+  $(document).ready(function () {
+    var scrollTop = 0;
+    $(window).scroll(function () {
+      scrollTop = $(window).scrollTop();
+      $('.counter').html(scrollTop);
+
+      if (scrollTop >= 100) {
+        $('.toolbar').addClass('scrolled');
+      } else if (scrollTop < 100) {
+        $('.toolbar').removeClass('scrolled');
+      }
+
+    });
+
+  }),
 
   <header className="toolbar">
     <nav className="toolbar_nav">
@@ -19,8 +37,7 @@ const Navbar = () => (
       <div id="MobileMenu" className="mNav" tabindex="0">
         <div className="mNav-dropdown">
         <a href="/">About</a>
-        <a href="/">Collabate</a>
-        <a href="/">Careers</a>
+        <a href="/">Collaborate</a>
         <a href="/">Contact</a>
         </div>
       </div>
@@ -31,8 +48,7 @@ const Navbar = () => (
       <div id="DesktopMenu" className="toolbar_nav_items">
         <ul>
           <li><a href="/">About</a></li>
-          <li><a href="/">Collabate</a></li>
-          <li><a href="/">Careers</a></li>
+          <li><a href="/">Collaborate</a></li>
           <li><a href="/">Contact</a></li>
         </ul>
       </div>
